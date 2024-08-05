cae163a88e7eadab4ba33ccd34f9da4b10257100
Sean Cunningham <sc.sean.cunningham@gmail.com>
2024-08-05 18:31:28 -0500

    update test.txt file with more text

--- ghi/src/Dropzone.jsx
+++ ghi/src/Dropzone.jsx
@@ -35,7 +35,6 @@ export default function Dropzone() {
   const onDrop = useCallback((acceptedFiles) => {
     acceptedFiles.forEach((file) => {
       const reader = new FileReader();
-      console.log("file accepted")
       reader.onabort = () => console.log('file reading was aborted');
       reader.onerror = () => console.log('file reading has failed');
       reader.onload = () => {
